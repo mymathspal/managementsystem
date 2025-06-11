edd970c749c752b989318f0376ff39ef647bd118
Nana Boateng <nanakab.dev@gmail.com>
2025-06-11 21:48:22 +0000

    comments added

--- php/index.php
+++ php/index.php
@@ -1,3 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=mysql;dbname=myapp', 'root', 'root');
-echo "Connected to the database successfully!<br>";
+//$pdo = new PDO('mysql:host=mysql;dbname=myapp', 'root', 'root');
+echo "Connected to the database successfully!<br>
+my name is Nana Kwame and this is my first PHP application.<br>
+<br>";
+
+?>
